8f759448b2b96588b781b2dc9be3ae6372f159d8
Robert San <robertsandru96@gmail.com>
2019-01-04 15:44:03 +0200

    Initial project structure and pods

---
+++ areyouin/View/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  areyouin
+//
+//  Created by Robert Sandru on 1/4/19.
+//  Copyright © 2019 Robert Sandru. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ areyouin/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  areyouin
+//
+//  Created by Robert Sandru on 1/4/19.
+//  Copyright © 2019 Robert Sandru. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
